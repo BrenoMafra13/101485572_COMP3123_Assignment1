780137cb3caaaebac0fe6306fe80c4c3783e1e2b
BrenoMafra13 <brenolopesmafra@gmail.com>
2025-09-25 10:34:14 -0400

    Adding getEmployees method

---
+++ controllers/employeeController.js
@@ -0,0 +1,21 @@
+const Employee = require('../models/Employee');
+const { validationResult } = require('express-validator');
+
+exports.getEmployees = async (req, res) => {
+  try {
+    const employees = await Employee.find();
+    const formatted = employees.map(e => ({
+      employee_id: e._id,
+      first_name: e.first_name,
+      last_name: e.last_name,
+      email: e.email,
+      position: e.position,
+      salary: e.salary,
+      date_of_joining: e.date_of_joining,
+      department: e.department
+    }));
+    res.status(200).json(formatted);
+  } catch (err) {
+    res.status(500).json({ status: false, message: err.message });
+  }
+};
