9f63fec32610f1aa833a4d6dbe5bf8d1cd700f52
BrenoMafra13 <brenolopesmafra@gmail.com>
2025-09-25 11:21:29 -0400

    Adding updateEmployee method

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -61,3 +61,21 @@ exports.getEmployeeById = async (req, res) => {
     res.status(500).json({ status: false, message: err.message });
   }
 };
+
+exports.updateEmployee = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ status: false, message: errors.array()[0].msg });
+  }
+  try {
+    const employee = await Employee.findByIdAndUpdate(req.params.eid, req.body, { new: true });
+    if (!employee) {
+      return res.status(404).json({ status: false, message: 'Employee not found' });
+    }
+    res.status(200).json({
+      message: 'Employee details updated successfully.'
+    });
+  } catch (err) {
+    res.status(500).json({ status: false, message: err.message });
+  }
+};
