f9f73b4a0c0a12631cfde952ccdf2cfac4703836
BrenoMafra13 <brenolopesmafra@gmail.com>
2025-09-25 11:43:13 -0400

    Adding deleteEmployee method and fixing employee routes

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -79,3 +79,21 @@ exports.updateEmployee = async (req, res) => {
     res.status(500).json({ status: false, message: err.message });
   }
 };
+
+exports.deleteEmployee = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ status: false, message: errors.array()[0].msg });
+  }
+  try {
+    const { employeeid } = req.query;
+    const employee = await Employee.findByIdAndDelete(employeeid);
+    if (!employee) {
+      return res.status(404).json({ status: false, message: 'Employee not found' });
+    }
+    res.status(204).send();
+  } catch (err) {
+    res.status(500).json({ status: false, message: err.message });
+  }
+};
+
--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const employeeController = require('../controllers/employeeController');
 
 router.get('/employees', employeeController.getEmployees);
-router.post('/employees', (req, res) => res.send('create employee ok'));
-router.get('/employees/:eid', (req, res) => res.send('get employee by id ok'));
-router.put('/employees/:eid', (req, res) => res.send('update employee ok'));
-router.delete('/employees', (req, res) => res.send('delete employee ok'));
+router.post('/employees', employeeController.createEmployee);
+router.get('/employees/:eid', employeeController.getEmployeeById);
+router.put('/employees/:eid', employeeController.updateEmployee);
+router.delete('/employees', employeeController.deleteEmployee);
 
 module.exports = router;
 
