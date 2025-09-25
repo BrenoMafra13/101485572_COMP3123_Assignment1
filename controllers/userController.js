22ccb9aa9d9e8ed470ab4927234e22df7db381bc
BrenoMafra13 <brenolopesmafra@gmail.com>
2025-09-25 09:49:39 -0400

    Adding SignUp method on userController.js

---
+++ controllers/userController.js
@@ -0,0 +1,22 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const { validationResult } = require('express-validator');
+
+exports.signup = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ status: false, message: errors.array()[0].msg });
+  }
+  try {
+    const { username, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ username, email, password: hashedPassword });
+    await user.save();
+    res.status(201).json({
+      message: 'User created successfully.',
+      user_id: user._id
+    });
+  } catch (err) {
+    res.status(500).json({ status: false, message: err.message });
+  }
+};
