557cbd8745e8e44e7a1f15790fc769208fa34104
BrenoMafra13 <brenolopesmafra@gmail.com>
2025-09-25 10:08:37 -0400

    Adding LogIn method on userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,3 +20,27 @@ exports.signup = async (req, res) => {
     res.status(500).json({ status: false, message: err.message });
   }
 };
+
+exports.login = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ status: false, message: errors.array()[0].msg });
+  }
+  try {
+    const { username, email, password } = req.body;
+    const user = await User.findOne({ $or: [{ email }, { username }] });
+    if (!user) {
+      return res.status(401).json({ status: false, message: 'Invalid Username and password' });
+    }
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      return res.status(401).json({ status: false, message: 'Invalid Username and password' });
+    }
+    res.status(200).json({
+      message: 'Login successful.',
+      jwt_token: 'Optional implementation'
+    });
+  } catch (err) {
+    res.status(500).json({ status: false, message: err.message });
+  }
+};
