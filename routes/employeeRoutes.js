67019de9164d60d242bb00a4d3895c4ebf78c7d8
BrenoMafra13 <brenolopesmafra@gmail.com>
2025-09-25 10:35:15 -0400

    Updating getEmployees method

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const employeeController = require('../controllers/employeeController');
 
-router.get('/employees', (req, res) => res.send('get employees ok'));
+router.get('/employees', employeeController.getEmployees);
 router.post('/employees', (req, res) => res.send('create employee ok'));
 router.get('/employees/:eid', (req, res) => res.send('get employee by id ok'));
 router.put('/employees/:eid', (req, res) => res.send('update employee ok'));
 router.delete('/employees', (req, res) => res.send('delete employee ok'));
 
 module.exports = router;
+
