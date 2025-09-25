1902021c90ea1c7a863128d403ac866815733610
BrenoMafra13 <brenolopesmafra@gmail.com>
2025-09-25 16:00:57 -0400

    Adding hosting on Vercel

--- server.js
+++ server.js
@@ -6,14 +6,11 @@ const employeeRoutes = require('./routes/employeeRoutes');
 const app = express();
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/comp3123_assignment1')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/comp3123_assignment1')
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
 app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/emp', employeeRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+module.exports = app;
